aba57d4bcc973123a86698561e14df06d32bcfc3
Tetrag11 <tehanekanayake@gmail.com>
2022-03-20 07:26:31 +0530

    sdas

--- dist/script.js
+++ dist/script.js
@@ -1,11 +1,56 @@
 gsap.registerPlugin(ScrollTrigger);
 
-const timeline = gsap.timeline({defaults: {duration: 1}});
 
-gsap.to('.animate-3', {scrollTrigger:'.animate-3', opacity: 1, duration: 1, delay: 0.5});
 
-timeline
-  .from('.lines', { y: '-150%', stagger: 0.1})
-  .from('.header-back-image', { y: '-150%'} )
-  .from('.animate-1', {opacity: 0},'<2')
-  .from('.animate-2', {opacity: 0}, '<1')
+
+
+const timeline = gsap.timeline({ defaults: { duration: 1 } });
+
+const timeline_2 = gsap.timeline({paused:true});
+
+timeline_2
+      .to('.nav-rel', {opacity: 0})
+      .to('.nav-line-left', {x:"0%", duration: 1, ease: Power3.easeIn}, 1)
+      .to('.nav-line-right', {x:"0%", duration: 1, ease: Power3.easeIn}, 1)
+      .to('.navbar', {x:"0%", duration: 2, stagger: 0.1});
+
+const timeline_3 = gsap.timeline();
+
+// loader related
+let load = false;
+let loadtime = false;
+let seconds = 0;
+
+window.addEventListener("load", (event) => {
+  load = true;
+});
+
+function increment() {
+  seconds += 1;
+  if (seconds > 3 && load == true) {
+    document.querySelector(".loader").style.display = "none";
+    document.querySelector(".body-content").style.display = "block";
+    timeline
+      .from(".lines", { y: "-150%", stagger: 0.1 })
+      .from(".header-back-image", { y: "-150%" })
+      .from(".animate-1", { opacity: 0 }, "<2")
+      .from(".animate-2", { opacity: 0 }, "<1")
+      .from(".animate-3", { opacity: 0 }, "<1");
+
+    clearInterval(secondscalc);
+  }
+}
+let secondscalc = setInterval(increment, 1000);
+
+
+// navBar
+
+function openNav(){
+  document.querySelector('.navbar-lines').style.zIndex = '100';
+   timeline_2.play();
+}
+
+function closeNav(){
+  document.querySelector('.navbar-lines').style.zIndex = '30';
+  timeline_2.reverse()
+}
